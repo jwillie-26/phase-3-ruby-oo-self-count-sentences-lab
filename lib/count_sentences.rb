7165c649ac995da38c7fd658fab270b60f120c18
jwillie-26 <105637783+jwillie-26@users.noreply.github.com>
2023-02-09 08:10:40 +0300

    completed assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+
+    self.end_with?(".") 
   end
 
   def question?
 
+    self.end_with?("?")
   end
 
   def exclamation?
 
+    self.end_with?("!")
   end
 
   def count_sentences
 
+    sentences = self.split(/[?!.]/)
+    sentences.filter { |sen| sen.match(/(\w+)/)}.count
   end
+
 end
